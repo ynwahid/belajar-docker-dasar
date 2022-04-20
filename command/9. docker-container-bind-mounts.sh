defd33291575c551af46502252c4210a3fe9614d
ynwahid <yusufnurwahid.contact@gmail.com>
2022-04-20 23:35:16 +0700

    container bind mount

---
+++ command/9. docker-container-bind-mounts.sh
@@ -0,0 +1,11 @@
+# Bind mounting (share drive)
+docker container create \
+    --name container-name \
+    --mount "type=bind,source=folder,destination=folder,readonly" \
+    image:tag
+# Example
+docker container create --name mongodata \
+    --mount "type=bind,source=/home/cakcup/Documents/mongo-data,destination=/data/db" \
+    --env MONGO_INITDB_ROOT_USERNAME=admin --env MONGO_INITDB_ROOT_PASSWORD=admin \
+    --publish 27018:27017 \
+    mongo:latest
