85f5ccca95a7ac64a49e852e31e466f64db0d308
ynwahid <yusufnurwahid.contact@gmail.com>
2022-04-20 13:35:26 +0700

    docker image

---
+++ command/1. docker-image.sh
@@ -0,0 +1,6 @@
+# List image docker in registry
+docker image ls
+# Download docker image, default tag = latest
+docker image pull redis:latest
+# Remove docker image
+docker image rm image-name:tag
