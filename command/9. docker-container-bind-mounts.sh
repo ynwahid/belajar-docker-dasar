144d5baac248436aa63dc686eb3fe5a1988d8aed
ynwahid <yusufnurwahid.contact@gmail.com>
2022-04-21 23:53:25 +0700

    change bind mounts source

--- command/9. docker-container-bind-mounts.sh
+++ command/9. docker-container-bind-mounts.sh
@@ -5,7 +5,7 @@ docker container create \
     image:tag
 # Example
 docker container create --name mongodata \
-    --mount "type=bind,source=/home/cakcup/Documents/mongo-data,destination=/data/db" \
+    --mount "type=bind,source=/home/cakcup/Documents/projects/belajar-docker-dasar/mongo-data,destination=/data/db" \
     --env MONGO_INITDB_ROOT_USERNAME=admin --env MONGO_INITDB_ROOT_PASSWORD=admin \
     --publish 27018:27017 \
     mongo:latest
