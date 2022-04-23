f8bd058e2a295309ab51316007aec4dd7924be57
ynwahid <yusufnurwahid.contact@gmail.com>
2022-04-23 14:06:53 +0700

    prune

---
+++ command/17. docker-prune.sh
@@ -0,0 +1,11 @@
+# Remove unused docker-things
+# Prune container
+docker container prune
+# Prune image
+docker image prune
+# Prune network
+docker network prune
+# Prune volume
+docker volume prune
+# Prune container, network, and image
+docker system prune
