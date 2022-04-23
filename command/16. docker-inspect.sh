5bdc2aacf5e41a6b77c17fd77a50f8939e1e1ab6
ynwahid <yusufnurwahid.contact@gmail.com>
2022-04-23 13:57:48 +0700

    inspect

---
+++ command/16. docker-inspect.sh
@@ -0,0 +1,9 @@
+# Get detail of docker
+# Inspect image
+docker image inspect image-name
+# Inspect container
+docker container inspect container-name
+# Inspect volume
+docker volume inspect volume-name
+# Inspect network
+docker network inspect network-name
